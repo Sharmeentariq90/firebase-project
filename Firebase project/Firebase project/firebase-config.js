6a34c107f9ae897272954fd9b220c014cb68ff86
Sharmeentariq90 <122076590+Sharmeentariq90@users.noreply.github.com>
2023-08-19 20:58:18 -1100

    Add files via upload

---
+++ Firebase project/Firebase project/firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAAK_gxgjZ47YS4VhY__8emAxx9IX-ZDnM",
+    authDomain: "my-first-firebase-projec-22d47.firebaseapp.com",
+    databaseURL: "https://my-first-firebase-projec-22d47-default-rtdb.firebaseio.com",
+    projectId: "my-first-firebase-projec-22d47",
+    storageBucket: "my-first-firebase-projec-22d47.appspot.com",
+    messagingSenderId: "540311308065",
+    appId: "1:540311308065:web:45824e6815d5a2899d6060"
+  };
